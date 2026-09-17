6b4813acdcc646d1be1e24864e0275bb645963c3
balcse <cserpesbalazs@gmail.com>
2026-09-17 09:48:32 +0200

    feat: created dynamic loading of allowed origins for CORS

--- src/app/auth/config.py
+++ src/app/auth/config.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_MINUTES: int
+    ALLOWED_ORIGINS: str  
 
     class Config:
         env_file = "app/.env"
